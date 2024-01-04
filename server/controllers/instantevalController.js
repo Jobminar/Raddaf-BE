6ec9edcc64559844993cf9ad5623545482b16d6b
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-01-04 10:49:25 +0530

    done changes in evaluation

--- server/controllers/instantevalController.js
+++ server/controllers/instantevalController.js
@@ -1,12 +1,17 @@
 // controllers/instantevalController.js
+
 import InstantEval, { find } from "../models/instanteval";
 
 // Get all instantevals
 export async function getAllInstantEvals(req, res) {
   try {
+    // Fetch all InstantEvals from the database
     const instantEvals = await find();
+
+    // Respond with the retrieved InstantEvals
     res.json(instantEvals);
   } catch (error) {
+    // Handle errors and respond with a 500 Internal Server Error
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
@@ -15,6 +20,7 @@ export async function getAllInstantEvals(req, res) {
 // Post a new instanteval
 export async function postInstantEval(req, res) {
   try {
+    // Destructure the relevant fields from the request body
     const {
       userId,
       username,
@@ -31,6 +37,7 @@ export async function postInstantEval(req, res) {
       bestTimeToContact,
     } = req.body;
 
+    // Create a new InstantEval instance with the provided data
     const newInstantEval = new InstantEval({
       userId,
       username,
@@ -47,9 +54,13 @@ export async function postInstantEval(req, res) {
       bestTimeToContact,
     });
 
+    // Save the new InstantEval record to the database
     await newInstantEval.save();
+
+    // Respond with a 201 Created status and a success message
     res.status(201).json({ message: "InstantEval created successfully" });
   } catch (error) {
+    // Handle errors and respond with a 500 Internal Server Error
     console.error(error);
     res.status(500).json({ error: "Internal Server Error" });
   }
