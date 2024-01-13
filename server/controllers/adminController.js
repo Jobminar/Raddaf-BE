0204a4040cee73d09b500aa3d72eefe13225a481
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-01-13 13:45:06 +0530

    added new controller adn route for admin login

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,109 +1,127 @@
 import Admin from "../models/Admin.js";
 import Agent from "../models/Agent.js";
-import {sendEmail} from "../services/sendEmailService.js"
+import { sendEmail } from "../services/sendEmailService.js";
 
 export async function saveAdmin(req, res) {
-    try {
+  try {
+    const { username, email, password, fullname, role, phoneNo, adminZones } =
+      req.body;
 
-        const { username, email, password, fullname, role, phoneNo, adminZones } = req.body;
-      
-        // validation should be here 
+    // validation should be here
 
+    // check if admin with same email already exist or not
+    const admin = await Admin.find({
+      $or: [{ email: email }, { phoneNo: phoneNo }],
+    });
 
+    console.log("admin found is", admin);
 
-        // check if admin with same email already exist or not 
-        const admin = await Admin.find({
-            $or: [
-                { email: email },
-                { phoneNo: phoneNo }
-            ]
-        });
-
-        console.log("admin found is", admin);
-
-        if (admin && admin.length > 0) {
-            return res.status(400).json({
-                error: "Admin With Email or Phone Number Already  Exist",
-            });
-        }
-
-        // Admin doesn't exist 
-
-        const insertAdmin = await new Admin({
-            "username": username, "email": email, "password": password, "fullname": fullname, "role": role, "phoneNo": phoneNo, "adminZones": adminZones
-        })
-
-        let result = await insertAdmin.save()
-
-        console.log("result of save operation is ", result);
-
-        // Provide success message
-        res.status(200).json({ message: "Admin Has Been Successfully Onboarded" });
-
-
-
-    } catch (error) {
-        console.log("Error in save Admin function", error);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (admin && admin.length > 0) {
+      return res.status(400).json({
+        error: "Admin With Email or Phone Number Already  Exist",
+      });
     }
+
+    // Admin doesn't exist
+
+    const insertAdmin = await new Admin({
+      username: username,
+      email: email,
+      password: password,
+      fullname: fullname,
+      role: role,
+      phoneNo: phoneNo,
+      adminZones: adminZones,
+    });
+
+    let result = await insertAdmin.save();
+
+    console.log("result of save operation is ", result);
+
+    // Provide success message
+    res.status(200).json({ message: "Admin Has Been Successfully Onboarded" });
+  } catch (error) {
+    console.log("Error in save Admin function", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
-export async function getAgentsListForApproval(req,res){
-    try{
-        const { adminZones } = req.body;
+export async function adminLogin(req, res) {
+  try {
+    const { email, password } = req.body;
 
-        if(adminZones && adminZones.length > 0){
+    // Find admin by email
+    const admin = await Admin.findOne({ email });
 
-            // find all agents who are not approved or rejected by admin
-            const getAgents = await Agent.find({
-               zoneNumber : {
-                "$in": adminZones
-               },
-               verified : false,
-               approvedOn : {
-                "$in": ["", null]
-               }
-            });
-
-            console.log("Agents" , getAgents);
-
-            if (getAgents && getAgents.length > 0) {
-
-              res.status(200).json({ msg: "Success", result: getAgents });
-            }else{
-                res.status(400).json({ msg: "NO Agents Pending for Approval" });
-            }
-        }
-
-
-    }catch(error){
-        console.log("Error in getAgentsListForApproval", getAgentsListForApproval);
-        res.status(500).json({ error: "Internal Server Error" });
+    if (!admin) {
+      return res.status(404).json({ error: "Admin not found" });
     }
+
+    // Validate password
+    const isValidPassword = await bcrypt.compare(password, admin.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ error: "Invalid password" });
+    }
+
+    // Admin successfully authenticated
+    res.status(200).json({ message: "Admin login successful" });
+  } catch (error) {
+    console.error("Error in adminLogin function", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
 }
 
-export async function approveAgent(req,res){
-    try{
-        const { email , verrifiedStatus } = req.body
+export async function getAgentsListForApproval(req, res) {
+  try {
+    const { adminZones } = req.body;
 
-        //validation here
-        sendEmail()
+    if (adminZones && adminZones.length > 0) {
+      // find all agents who are not approved or rejected by admin
+      const getAgents = await Agent.find({
+        zoneNumber: {
+          $in: adminZones,
+        },
+        verified: false,
+        approvedOn: {
+          $in: ["", null],
+        },
+      });
 
-        // const updateResult =  await Agent.updateOne({ email : email} , {
-        //     "$set": {
-        //         verified : verrifiedStatus,
-        //         approvedOn : new Date().toISOString()
-        //     } }, { new : true}
-        // )
-        // console.log("update result is" , updateResult);
-        // if(updateResult){
-        //     res.status(200).json({ msg: "Verified Status Has Been Successfully Updated" });
-        // }else{
-        //     res.status(400).json({ msg: "Unable to update the Agent" });
-        // }
+      console.log("Agents", getAgents);
 
-
-    }catch(error){
-        console.log("Error in approveAgent Code", error);
+      if (getAgents && getAgents.length > 0) {
+        res.status(200).json({ msg: "Success", result: getAgents });
+      } else {
+        res.status(400).json({ msg: "NO Agents Pending for Approval" });
+      }
     }
+  } catch (error) {
+    console.log("Error in getAgentsListForApproval", getAgentsListForApproval);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
+
+export async function approveAgent(req, res) {
+  try {
+    const { email, verrifiedStatus } = req.body;
+
+    //validation here
+    sendEmail();
+
+    // const updateResult =  await Agent.updateOne({ email : email} , {
+    //     "$set": {
+    //         verified : verrifiedStatus,
+    //         approvedOn : new Date().toISOString()
+    //     } }, { new : true}
+    // )
+    // console.log("update result is" , updateResult);
+    // if(updateResult){
+    //     res.status(200).json({ msg: "Verified Status Has Been Successfully Updated" });
+    // }else{
+    //     res.status(400).json({ msg: "Unable to update the Agent" });
+    // }
+  } catch (error) {
+    console.log("Error in approveAgent Code", error);
+  }
 }
--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,16 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { saveAdmin, getAgentsListForApproval , approveAgent } from "../controllers/adminController.js";
-
+import {
+  saveAdmin,
+  getAgentsListForApproval,
+  approveAgent,
+  adminLogin,
+} from "../controllers/adminController.js";
 
 router.post("/save", saveAdmin);
-
+router.post("/login", adminLogin);
 router.post("/getAgentList", getAgentsListForApproval);
 
-router.post("/approveAgent", approveAgent )
-
-
-
+router.post("/approveAgent", approveAgent);
 
 export default router;
-
